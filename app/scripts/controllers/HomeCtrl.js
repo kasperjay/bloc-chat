1787c7805db6ac3243bb9f72d6aac6b8e78bfc36
Kasper Pickett <kasperpickett@gmail.com>
2018-05-20 03:00:39 -0500

    checkpoint 3 in progress. added message service

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,14 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal) {
         var home = this;
         this.rooms = Room.all;
+        this.currentRoom = null;
+
+        this.setCurrentRoom = function (room) {
+            this.currentRoom = room;
+            this.messages = Message.getByRoomId(this.currentRoom.$id);
+            console.log(home.messages)
+        }
 
       home.addRoom = function() {
           $uibModal.open({
@@ -9,10 +16,11 @@
             size: 'sm',
             controller: 'ModalCtrl as modal'
           });
-      }
+        }
+
   }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
