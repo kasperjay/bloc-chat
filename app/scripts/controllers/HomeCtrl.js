c8f869f3f50703688241cdf776050e7b5e008c91
Kasper Pickett <kasperpickett@gmail.com>
2018-05-20 00:08:15 -0500

    added create room modal.

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,18 @@
 (function() {
-    function HomeCtrl(Room) {
-      this.rooms = Room.all;
-    }
+    function HomeCtrl(Room, $uibModal) {
+        var home = this;
+        this.rooms = Room.all;
+
+      home.addRoom = function() {
+          $uibModal.open({
+            templateUrl: '/templates/modal.html',
+            size: 'sm',
+            controller: 'ModalCtrl as modal'
+          });
+      }
+  }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
 })();
---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,17 @@
+(function() {
+    function ModalCtrl(Room, $uibModalInstance, $cookies) {
+        var modal = this;
+        modal.cancel = function () {
+            $uibModalInstance.dismiss();
+        };
+
+        modal.createRoom = function () {
+            Room.add(modal.newRoom);
+            $uibModalInstance.close();
+        }
+      }
+
+    angular
+      .module('blocChat')
+      .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl]);
+    })();
